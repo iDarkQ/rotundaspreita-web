bd51ea3ce301947d668fb8e447f4c672e0a3036c
iDarkQ <matikuki3@gmail.com>
2025-11-19 08:04:29 +0000

    feat(fetch-logged-user.ts): create fetch logged user server function

---
+++ app/_server/fetch-logged-user.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import { authenticate } from "@/services/user-service";
+import { cookies } from "next/headers";
+
+export const fetchLoggedUser = async () => {
+    const cookie = await cookies();
+    const jwtToken = cookie.get("secret");
+
+    const user = jwtToken ? await authenticate(jwtToken.value) : null;
+    return user;
+}
