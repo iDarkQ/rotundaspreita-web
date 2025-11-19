7657b544d864ce24be1476f3f0f7ccd6b3a2c4cc
iDarkQ <matikuki3@gmail.com>
2025-11-19 07:56:28 +0000

    refactor(section.tsx): justify content to the start & reduce padding vertically

--- app/_components/section.tsx
+++ app/_components/section.tsx
@@ -10,7 +10,7 @@ export const Section = ({ children, className }: SectionProps) => (
   <section className="relative min-h-screen w-full flex justify-center bg-background overflow-hidden">
     <div
       className={clsx(
-        "flex flex-col items-center justify-center w-full max-w-full sm:max-w-[90%] md:max-w-[80%] xl:max-w-7xl mx-auto gap-8 py-40",
+        "flex flex-col items-center justify-start w-full max-w-full sm:max-w-[90%] md:max-w-[80%] xl:max-w-7xl mx-auto gap-8 my-30",
         className && className
       )}
     >
