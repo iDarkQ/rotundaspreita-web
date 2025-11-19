a75e2841cc3ee48202a447600f4cbce8ffd9c85a
iDarkQ <matikuki3@gmail.com>
2025-11-19 08:01:35 +0000

    refactor(chip.tsx): finish layout & add variants & add onClick

--- app/_components/chip.tsx
+++ app/_components/chip.tsx
@@ -1,9 +1,34 @@
-import { ReactNode } from "react";
+"use client";
+
+import { useRipple } from "@/app/_hooks/use-ripple";
+import clsx from "clsx";
+import { ReactNode, RefObject } from "react";
 
 interface Props {
   children: ReactNode;
+  variant?: "contained" | "outlined";
+  onClick?: () => void;
+  clickable?: boolean;
 }
 
-export const Chip = ({ children }: Props) => {
-  return <div className="bg-primary rounded-xl px-3 h-(--chip-height) flex items-center justify-center">{children}</div>;
+export const Chip = ({ children, onClick, clickable, variant = "contained" }: Props) => {
+  const { events, cancel, ref } = useRipple(true, (!onClick && !clickable));
+
+  return (
+    <div
+      ref={ref as RefObject<HTMLDivElement | null>}
+      onPointerDown={events}
+      onPointerUp={events}
+      onMouseLeave={cancel}
+      onClick={onClick}
+      className={clsx(
+        "relative overflow-hidden rounded-xl px-3 h-(--chip-height) flex items-center justify-center transition-colors",
+        variant === "contained" && "bg-primary",
+        variant === "outlined" && "c-border",
+        (onClick || clickable) && "cursor-pointer select-none"
+      )}
+    >
+      {children}
+    </div>
+  );
 };
