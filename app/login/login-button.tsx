d648161e8a5a760724930bad81c8f89796183700
iDarkQ <matikuki3@gmail.com>
2025-11-19 08:04:42 +0000

    feat(login-button.tsx): create login button component

---
+++ app/login/login-button.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Button } from "@/app/_components/button";
+import { Text } from "@/app/_components/text";
+import { registerUser } from "@/services/user-service";
+import { useGoogleLogin } from "@react-oauth/google";
+import { FaGoogle } from "react-icons/fa";
+import { redirect } from "next/navigation";
+import { saveSecret } from "@/app/login/save-secret";
+
+interface Props {
+  ip: string;
+  agent: string;
+}
+
+export const LoginButton = ({ ip, agent }: Props) => {
+  const login = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      const secret = await registerUser(tokenResponse, ip, agent);
+      saveSecret(secret ?? "");
+      redirect("/panel");
+    },
+  });
+
+  return (
+    <Button
+      className="gap-2 flex flex-row items-center justify-center"
+      onClick={() => login()}
+    >
+      <Text as="h4" className="text-white">
+        Authenticate with Google{" "}
+      </Text>
+      <FaGoogle className="text-white" size={20} />
+    </Button>
+  );
+};
