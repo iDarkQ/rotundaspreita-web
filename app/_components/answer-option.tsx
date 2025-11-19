6b30b0463e96052d04d1adad4ba012662a118a56
iDarkQ <matikuki3@gmail.com>
2025-11-19 08:00:33 +0000

    refactor(answer-option.tsx): add editable state

--- app/_components/answer-option.tsx
+++ app/_components/answer-option.tsx
@@ -6,14 +6,38 @@ import { Label } from "@headlessui/react";
 interface Props {
   option: string;
   label: string;
+  defaultValue?: string;
   onClick?: () => void;
+  onDelete?: () => void;
+  onChange?: (text: string) => void;
   selected?: boolean;
+  editable?: boolean;
 }
 
-export const AnswerOption = ({ option, label, onClick, selected }: Props) => (
-  <RadioButton value={label} selected={selected} onClick={onClick}>
+export const AnswerOption = ({
+  option,
+  label,
+  defaultValue,
+  onClick,
+  onDelete,
+  onChange,
+  editable = false,
+  selectable = true,
     <Text as="h2">{option}</Text>
     <Divider orientation="vertical" />
-    <Label className="text-base text-neutral flex-1">{label}</Label>
+    {editable ? (
+      <Input
+        className="border-hidden! outline-hidden! w-full"
+        onClick={(e) => e.stopPropagation()}
+        onFocus={(e) => e.stopPropagation()}
+        onPointerDown={(e) => e.stopPropagation()}
+        onPointerUp={(e) => e.stopPropagation()}
+        onChange={(e) => onChange?.(e.target.value)}
+        defaultValue={defaultValue}
+        placeholder="Click and type to write answer..."
+      />
+    ) : (
+      <Label className="text-base text-neutral flex-1">{label}</Label>
+    )}
   </RadioButton>
 );
