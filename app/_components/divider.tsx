47c517c3b5fc851f76951822005a3429109af696
iDarkQ <matikuki3@gmail.com>
2025-11-19 07:57:24 +0000

    refactor(divider.tsx): make divider orientation optional

--- app/_components/divider.tsx
+++ app/_components/divider.tsx
@@ -1,9 +1,18 @@
 import clsx from "clsx";
 
 interface Props {
-  orientation: "vertical" | "horizontal";
+  orientation?: "vertical" | "horizontal";
 }
 
 export const Divider = ({ orientation = "horizontal" }: Props) => {
-  return <div className={clsx("c-border", orientation === "horizontal" ? "h-0 w-full border-t-0!" : "w-0 h-full border-l-0!")} />;
+  return (
+    <div
+      className={clsx(
+        "c-border",
+        orientation === "horizontal"
+          ? "h-0 w-full border-t-0!"
+          : "w-0 h-full border-l-0!"
+      )}
+    />
+  );
 };
