0369b96d1c23ee6fe384faceb66a184dcb6be1a3
iDarkQ <matikuki3@gmail.com>
2025-11-20 08:11:43 +0000

    feat(questions-dialog.tsx): create QuestionsDialog component

---
+++ app/questions/[[...studyId]]/components/questions-dialog.tsx
@@ -0,0 +1,27 @@
+import { Dialog } from "@/app/_components/dialog";
+import { Text } from "@/app/_components/text";
+import { Option } from "@/app/generated/prisma/browser";
+
+interface Props {
+  question: string;
+  options: Option[];
+  onClose: () => void;
+}
+
+export const QuestionsDialog = ({ onClose, question, options }: Props) => {
+  return (
+    <Dialog onClose={onClose} title={question}>
+      <div className="flex flex-row gap-5  items-start justify-start">
+        {options.map((option, index) => (
+          <div
+            key={index}
+            className="flex-1 flex flex-col gap-1 items-start justify-center"
+          >
+            <Text as="h3">{option.letter}</Text>
+            <Text as="p">{option.content}</Text>
+          </div>
+        ))}
+      </div>
+    </Dialog>
+  );
+};
