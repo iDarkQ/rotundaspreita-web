3e443dca7cf0d8f828b3064279ceacd4b8955770
iDarkQ <matikuki3@gmail.com>
2025-11-22 08:18:22 +0000

    feat(test-countdown.tsx): create new component

---
+++ app/test/components/test-countdown.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Text } from "@/app/_components/text";
+import { useTestManager } from "@/app/test/providers/test-manager";
+
+export const TestCountdown = () => {
+  const { finished, finishTest } = useTestManager();
+
+  const [timeLeft, setTimeLeft] = useState(60 * 30);
+
+  useEffect(() => {
+    if (finished) return;
+    if (timeLeft <= 0) {
+      finishTest();
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setTimeLeft((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [timeLeft, finishTest, finished]);
+
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+
+  return (
+    <Text as="p" className="text-primary">
+      {minutes}:{seconds.toString().padStart(2, "0")}
+    </Text>
+  );
+};
