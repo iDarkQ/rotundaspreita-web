08ed8dfa108c061b30bd8c9c191b275a33fa4030
iDarkQ <matikuki3@gmail.com>
2025-11-22 08:22:40 +0000

    refactor(page.tsx): simplify, reuse newly created components & use real data about study, category and difficulty

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,58 +1,72 @@
-"use client";
-
-import { QuestionsPagination } from "@/app/_components/questions-pagination";
 import { Section } from "@/app/_components/section";
 import { Blob1 } from "@/app/_components/svgs/blob-1";
 import { Blob4 } from "@/app/_components/svgs/blob-4";
 import { PositionedBlob } from "@/app/_components/svgs/positioned-blob";
-import { useState } from "react";
-import { MdHourglassTop } from "react-icons/md";
+import { MdAlarm } from "react-icons/md";
 import { Text } from "@/app/_components/text";
-import { data, TestOptions } from "@/app/test/test-options";
-import { Divider } from "@/app/_components/divider";
 import { Chip } from "@/app/_components/chip";
-import { Button } from "@/app/_components/button";
+import { TestOptions } from "@/app/test/test-options";
+import { fetchStudyById, generateTest } from "@/services/study-service";
+import { Difficulty } from "@/types/difficulty";
+import { TestManagerProvider } from "@/app/test/providers/test-manager";
+import { TestCountdown } from "@/app/test/components/test-countdown";
+import { redirect } from "next/navigation";
+import { TestFinishButton } from "@/app/test/components/test-finish-button";
+import { TestResults } from "@/app/test/components/test-results";
 
-export default function Test() {
-  const [selected, setSelected] = useState<number>(1);
+interface Props {
+  searchParams?: Promise<{ s?: string; c?: string; d?: string }>;
+}
+
+export default async function Test({ searchParams }: Props) {
+  const {
+    s: selectedStudy,
+    c: selectedCategory,
+    d: rawDifficulty,
+  } = (await searchParams) ?? {};
+
+  const selectedDifficulty =
+    rawDifficulty && rawDifficulty in Difficulty
+      ? Difficulty[rawDifficulty as keyof typeof Difficulty]
+      : undefined;
+
+  const questions = await generateTest(selectedStudy, selectedDifficulty);
+
+  if (!questions || questions.length < 1) {
+    redirect("/panel");
+  }
+
+  const firstQuestion = questions[0];
+  const study = await fetchStudyById(firstQuestion.studyId);
 
   return (
-    <Section>
-      <PositionedBlob align="left" className="w-100 h-100 top-[90%]">
-        <Blob1 />
-      </PositionedBlob>
-      <PositionedBlob align="right" className="w-100 h-100 top-[10%]">
-        <Blob4 />
-      </PositionedBlob>
-      <div className="flex gap-2 items-center justify-center">
-        <Chip>
-          <Text as="p" className="text-white">
-            Teste de diretor
-          </Text>
-        </Chip>
-        <div className="flex items-center justify-center">
-          <Text as="p" className="text-primary">
-            30:00
-          </Text>
-          <MdHourglassTop className="text-primary" />
+    <TestManagerProvider questions={questions}>
+      <Section>
+        <PositionedBlob align="left" className="w-100 h-100 top-[70vh]">
+          <Blob1 />
+        </PositionedBlob>
+        <PositionedBlob align="right" className="w-100 h-100 top-[-10vh]">
+          <Blob4 />
+        </PositionedBlob>
+        <TestResults />
+        <div className="flex gap-2 items-center justify-center">
+          <Chip>
+            <Text as="p" className="text-white">
+              Teste de {study?.title}{" "}
+              {firstQuestion.category === selectedCategory
+                ? `(${selectedCategory})`
+                : ""}
+            </Text>
+          </Chip>
+          <div className="flex items-center justify-center gap-0.5">
+            <TestCountdown />
+            <MdAlarm className="text-primary" />
+          </div>
         </div>
-      </div>
-      <Text as="h1">{data[selected - 1].question}</Text>
-      <Divider orientation="horizontal" />
-      <div className="flex flex-col w-full gap-1">
-        <TestOptions selected={selected} setSelected={setSelected} />
-      </div>
 
-      <QuestionsPagination
-        count={data.length}
-        page={selected}
-        onChange={setSelected}
-      />
-      <Button>
-        <Text as="h3" className="text-white">
-          Finalizar Teste
-        </Text>
-      </Button>
-    </Section>
+        <TestOptions />
+        <TestFinishButton />
+      </Section>
+    </TestManagerProvider>
   );
 }
