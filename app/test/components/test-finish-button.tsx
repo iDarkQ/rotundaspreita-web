750cc8be832c5bc541d9ee169dac0168bf7ad14c
iDarkQ <matikuki3@gmail.com>
2025-11-22 08:18:30 +0000

    feat(test-finish-button.tsx): create new component

---
+++ app/test/components/test-finish-button.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Button } from "@/app/_components/button";
+import { Text } from "@/app/_components/text";
+import { TestFinishDialog } from "@/app/test/components/dialogs/test-finish-dialog";
+import { useTestManager } from "@/app/test/providers/test-manager";
+import { useState } from "react";
+
+export const TestFinishButton = () => {
+  const { finishTest, finished, answers } = useTestManager();
+  const [open, setOpen] = useState(false);
+
+  const handleButtonClick = () => {
+    if (finished) {
+      window.location.reload();
+      return;
+    }
+
+    const finishedAllQuestions = !Object.values(answers).includes(undefined);
+
+    if (!finishedAllQuestions) {
+      setOpen(true);
+      return;
+    }
+
+    finishTest();
+  };
+
+  return (
+    <>
+      <Button onClick={handleButtonClick}>
+        <Text as="h3" className="text-white">
+          {finished ? "Faça Outro Teste" : "Finalizar Teste"}
+        </Text>
+      </Button>
+      {open && <TestFinishDialog onClose={() => setOpen(false)} />}
+    </>
+  );
+};
