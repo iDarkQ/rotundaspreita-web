c53df182de8159f2a49ab87cffc4ee78e2940c96
iDarkQ <matikuki3@gmail.com>
2025-11-22 08:21:51 +0000

    refactor(test-options.tsx): display correct and wrong answers once test if finished

--- app/test/test-options.tsx
+++ app/test/test-options.tsx
@@ -1,115 +1,97 @@
+"use client";
+
 import { AnswerOption } from "@/app/_components/answer-option";
+import { Divider } from "@/app/_components/divider";
+import { QuestionsPagination } from "@/app/_components/questions-pagination";
+import { Text } from "@/app/_components/text";
+import { useTestManager } from "@/app/test/providers/test-manager";
 import { RadioGroup } from "@headlessui/react";
-import { Dispatch, SetStateAction, useState } from "react";
+import { useState } from "react";
 
-interface Props {
-  selected: number;
-  setSelected: Dispatch<SetStateAction<number>>;
-}
+export const TestOptions = () => {
+  const {
+    answers,
+    setAnswers,
+    questions,
+    finished,
+    correctAnswers,
+    selectedPage,
+    setSelectedPage,
+  } = useTestManager();
 
-export const TestOptions = ({ selected, setSelected }: Props) => {
-  const [answers, setAnswers] = useState<{ [key: number]: string }>({});
+  const question = questions.find((q) => q.id === selectedPage);
+
+  const currentIndex = questions.findIndex((q) => q.id === selectedPage);
+  const currentPage = currentIndex === -1 ? 0 : currentIndex + 1;
 
   return (
-    <RadioGroup className="flex flex-col gap-2">
-      {data[selected - 1].options.map((option) => (
-        <AnswerOption
-          key={option.option}
-          option={option.option}
-          label={option.label}
-          selected={
-            answers[selected] ? answers[selected] === option.option : false
-          }
-          onClick={() => {
-            if (selected < data.length && !answers[selected]) {
-              setSelected((prev) => prev + 1);
-            }
-            setAnswers((prev) => ({ ...prev, [selected]: option.option }));
-          }}
-        />
-      ))}
-    </RadioGroup>
+    <>
+      <div className="flex flex-col w-full min-h-[50vh] gap-8 items-center">
+        <Text as="h3">{question?.content}</Text>
+        <Divider orientation="horizontal" />
+        <div className="flex flex-col w-full gap-2">
+          <RadioGroup className="flex flex-col gap-2">
+            {question?.options.map((option) => {
+              const userAnswer = answers[selectedPage]; // what the user selected (might be undefined)
+              const correctAnswerId = correctAnswers[selectedPage]; // the correct option id
+
+              console.log({ userAnswer, correctAnswerId });
+              let state: "correct" | "wrong" | undefined = undefined;
+
+              if (finished) {
+                if (userAnswer === option.id) {
+                  state = userAnswer === correctAnswerId ? "correct" : "wrong";
+                }
+
+                if (option.id === correctAnswerId) {
+                  state = "correct";
+                }
+              }
+
+              return (
+                <AnswerOption
+                  key={option.id}
+                  option={option.letter}
+                  label={option.content}
+                  selected={
+                    !finished
+                      ? answers[selectedPage]
+                        ? answers[selectedPage] === option.id
+                        : false
+                      : undefined
+                  }
+                  selectable={!finished}
+                  state={state}
+                  onClick={() => {
+                    if (finished) return;
+                    if (
+                      currentPage < questions.length &&
+                      !answers[selectedPage]
+                    ) {
+                      setTimeout(() => {
+                        const nextQuestionId = questions[currentIndex + 1];
+                        setSelectedPage(nextQuestionId.id);
+                      }, 500);
+                    }
+
+                    setAnswers((prev) => ({
+                      ...prev,
+                      [selectedPage]: option.id,
+                    }));
+                  }}
+                />
+              );
+            })}
+          </RadioGroup>
+        </div>
+      </div>
+      <QuestionsPagination
+        count={questions.length}
+        page={currentPage}
+        onChange={(index) => {
+          setSelectedPage(questions[index - 1]?.id);
+        }}
+      />
+    </>
   );
 };
-
-export const data = [
-  {
-    question: "Qual a frase que melhor define previsão?",
-    answer: "A",
-    options: [
-      {
-        option: "A",
-        label:
-          "Imaginar o que pode acontecer, a partir dos índices pertinentes",
-      },
-      {
-        option: "B",
-        label: "Classificação dos índices em categorias com significado",
-      },
-      {
-        option: "C",
-        label:
-          "Conjunto de procedimentos do condutor para detetar a presença ou ausência de índices",
-      },
-    ],
-  },
-  {
-    question: "O sentido vestibular permite:",
-    answer: "B",
-    options: [
-      {
-        option: "A",
-        label: "A visão periférica.",
-      },
-      {
-        option: "B",
-        label:
-          "A manutenção do equilíbrio e orientação da posição de cada parte do corpo.",
-      },
-      {
-        option: "C",
-        label: "A perceção da velocidade de circulação dos veículos.",
-      },
-    ],
-  },
-  {
-    question:
-      "O desempenho do condutor durante a atividade da condução, pode ser",
-    answer: "C",
-    options: [
-      {
-        option: "A",
-        label:
-          "A fadiga, a idade, as drogas, o álcool, os conflitos psicológicos, as condições atmosféricas adversas.",
-      },
-      {
-        option: "B",
-        label: "A sonolência, a idade, a fadiga, as doenças",
-      },
-      {
-        option: "C",
-        label:
-          "A idade, a fadiga, as doenças, os conflitos psicológicos, as drogas, o álcool, o estado da via e a estabilidade do veículo",
-      },
-    ],
-  },
-  {
-    question:
-      "No processo da condução, em que consiste a exploração preceptiva visual?",
-    answer: "C",
-    options: [
-      {
-        option: "A",
-        label: "Na excitação dos órgãos sensoriais",
-      },
-      {
-        option: "B",
-        label: "Na deslocação dos olhos na cena visual.",
-      },
-      {
-        option: "C",
-        label: "No reconhecimento de objetos a partir de estímulos.",
-      },
-    ],
-  },
-];
