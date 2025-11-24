f95d64bbe60e8ce88063df1e396f8d5aba1a6093
iDarkQ <matikuki3@gmail.com>
2025-11-24 20:31:19 +0000

    feat(feature-analytics.tsx): create analytics feature for about page

---
+++ app/_components/home/features/feature-analytics.tsx
@@ -0,0 +1,47 @@
+import { TestFeatureCard } from "@/app/_components/home/test-feature-card";
+import { StatisticsCard } from "@/app/_components/statistics-card";
+import { Tilt } from "@/app/_components/tilt";
+import { PanelStatisticsChart } from "@/app/panel/[[...studyId]]/components/panel-statistics/panel-statistics-chart";
+import { MdPieChart } from "react-icons/md";
+
+export const FeatureAnalytics = async () => {
+  return (
+    <TestFeatureCard
+      prefix={<MdPieChart size={50} className="text-primary!" />}
+      title="Estatísticas Modernas"
+      description="Painéis que mostram progresso, tempo por questão e acertos."
+    >
+      <Tilt className="w-full">
+        <div className="flex gap-2 items-stretch w-full">
+          <div className="flex flex-col gap-2 w-full h-auto">
+            <StatisticsCard
+              name="Testes concluídos"
+              value="11"
+              className="flex-1 justify-center"
+            />
+            <StatisticsCard
+              name="Resultados médios dos testes %"
+              value="95%"
+              className="flex-1 justify-center"
+            />
+            <StatisticsCard
+              name="Perguntas vistas %"
+              value="41%"
+              className="flex-1 justify-center"
+            />
+          </div>
+          <PanelStatisticsChart
+            data={[
+              { label: "Perguntas respondidas", data: 340, color: "#326082" },
+              {
+                label: "Perguntas não respondidas",
+                data: 520,
+                color: "#e9c46a",
+              },
+            ]}
+          />
+        </div>
+      </Tilt>
+    </TestFeatureCard>
+  );
+};
