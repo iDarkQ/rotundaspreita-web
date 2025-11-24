6c522085d556988957149474c1e3686bdf26fdc1
iDarkQ <matikuki3@gmail.com>
2025-11-24 20:36:25 +0000

    refactor(list-box-options.tsx): move className prop to the bottom of clsx list

--- app/_components/list-box/list-box-options.tsx
+++ app/_components/list-box/list-box-options.tsx
@@ -10,9 +10,9 @@ export const ListBoxOptions = ({ ...rest }: ListboxOptionsProps) => (
     transition
     modal={false}
     className={clsx(
-      rest.className,
       "transition duration-200! data-leave:data-closed:opacity-0 data-enter:opacity-1",
-      "focus-visible:outline-none c-border border-transparent bg-tab-background! w-(--button-width) flex flex-col p-2 gap-2 [--anchor-gap:--spacing(1)]"
+      "focus-visible:outline-none c-border border-transparent bg-tab-background! w-(--button-width) flex flex-col p-2 gap-2 [--anchor-gap:--spacing(1)]",
+      rest.className
     )}
   >
     {rest.children}
