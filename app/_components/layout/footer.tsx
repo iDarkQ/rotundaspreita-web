437f9f1729a2460b6f6219142b23666b48bcb9db
iDarkQ <matikuki3@gmail.com>
2025-11-24 20:34:33 +0000

    feat(footer.tsx): add Contact link

--- app/_components/layout/footer.tsx
+++ app/_components/layout/footer.tsx
@@ -1,21 +1,24 @@
 import { Link } from "@/app/_components/link";
 import { Text } from "@/app/_components/text";
 
-export const Footer = () => {
-  return (
-    <div className="h-auto bg-primary flex flex-col items-center justify-center gap-2 p-10">
-      <div>
-        <ul>
-          <nav>
-            <li>
-              <Link link="/privacy-policy">Privacy Policy</Link>
-            </li>
-          </nav>
-        </ul>
-      </div>
-      <Text as="p" className="text-center text-2xl font-bold text-white">
-        Copyright © ROTUNDÁSPREITA | All rights reserved
-      </Text>
-    </div>
-  );
-};
+export const Footer = () => (
+  <div className="relative h-auto bg-primary flex flex-col items-center justify-center gap-2 p-10 z-1000">
+    <ul className="flex gap-2">
+      <nav>
+        <li>
+          <Link link="/privacy-policy">Política de Privacidade</Link>
+        </li>
+      </nav>
+      <nav>
+        <li>
+          <Link link="mailto:rotundaspreita@gmail.com" isExternalLink={true}>
+            Contacto
+          </Link>
+        </li>
+      </nav>
+    </ul>
+    <Text as="p" className="text-center text-2xl font-bold text-white">
+      Copyright © ROTUNDÁSPREITA | All rights reserved
+    </Text>
+  </div>
+);
