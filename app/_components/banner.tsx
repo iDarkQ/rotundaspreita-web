88ca581576e9ccb1ec06ca8a6ccfed572966ce5c
iDarkQ <matikuki3@gmail.com>
2025-11-25 20:41:02 +0000

    refactor(banner.tsx): add center prop that allows banner text to be easiely centered

--- app/_components/banner.tsx
+++ app/_components/banner.tsx
@@ -3,12 +3,13 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  center?: boolean;
   className?: string;
 }
 
-export const Banner = ({ children, className }: Props) => (
+export const Banner = ({ children, center, className }: Props) => (
   <div
-    className={clsx("c-border bg-white shadow-lg px-10 py-5", className && className)}
+    className={clsx("c-border bg-white shadow-lg px-10 py-5", center && "flex items-center justify-center",className && className)}
   >
     {children}
   </div>
