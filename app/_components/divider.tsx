07a3b62321ecd5c44313decfc19953f85b01ff02
iDarkQ <matikuki3@gmail.com>
2025-11-25 20:44:50 +0000

    refactor(divider.tsx): allow for className customization & simplify vertical divider styles

--- app/_components/divider.tsx
+++ app/_components/divider.tsx
@@ -2,16 +2,18 @@ import clsx from "clsx";
 
 interface Props {
   orientation?: "vertical" | "horizontal";
+  className?: string;
 }
 
-export const Divider = ({ orientation = "horizontal" }: Props) => {
+export const Divider = ({ className, orientation = "horizontal" }: Props) => {
   return (
     <div
       className={clsx(
         "c-border",
         orientation === "horizontal"
           ? "h-0 w-full border-t-0!"
-          : "w-0 h-full border-l-0!"
+          : "h-full border-l-0!",
+        className && className
       )}
     />
   );
