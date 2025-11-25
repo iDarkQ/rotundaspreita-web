bc7781cd935aaf3c2ffe9dff6177042a44fcdebf
iDarkQ <matikuki3@gmail.com>
2025-11-25 20:41:54 +0000

    refactor(use-ripple.tsx): create loadingAnimation

--- app/_hooks/use-ripple.ts
+++ app/_hooks/use-ripple.ts
@@ -60,5 +60,35 @@ export const useRipple = (
     }
   };
 
-  return { ref, events, cancel };
+  const loadingAnimation = () => {
+    const ripple = document.createElement("span");
+    ripple.className = clsx(
+      "absolute rounded-full [transform:scale(0)] transition-transform duration-1000 pointer-events-none opacity-100 z-1",
+      filled ? "bg-black/10" : "bg-primary"
+    );
+
+    const element = ref.current;
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect();
+    const size = Math.max(rect.width, rect.height);
+    ripple.style.width = ripple.style.height = `${size}px`;
+
+    const x = rect.width / 2 - size / 2;
+    const y = rect.height / 2 - size / 2;
+
+    ripple.style.left = `${x}px`;
+    ripple.style.top = `${y}px`;
+
+    element.appendChild(ripple);
+
+    setTimeout(() => {
+      rippleRef.current = ripple;
+
+      ripple.classList.add("[transform:scale(4)]");
+      cancel();
+    }, 10);
+  }
+
+  return { ref, events, cancel, loadingAnimation };
 };
