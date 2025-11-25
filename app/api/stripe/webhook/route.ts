ada9a4dd29b502ec0757df33c4bc49124387b807
iDarkQ <matikuki3@gmail.com>
2025-11-25 20:51:37 +0000

    feat(route.ts): create a stripe webhook handler

---
+++ app/api/stripe/webhook/route.ts
@@ -0,0 +1,36 @@
+import { startSubscription } from "@/services/server/subscription";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY!);
+
+export const POST = async (req: NextRequest) => {
+    const sig = req.headers.get("stripe-signature")!;
+    const body = await req.text();
+
+    let event: Stripe.Event;
+
+    try {
+        event = stripe.webhooks.constructEvent(
+            body,
+            sig,
+            process.env.STRIPE_WEBHOOK_SECRET!
+        );
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+        return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
+    }
+
+    switch (event.type) {
+        case "checkout.session.completed":
+            const session = event.data.object as Stripe.Checkout.Session;
+            const userId = session.metadata?.userId;
+            const subscriptionId = session.subscription as string;
+            if (!userId) return;
+
+            await startSubscription(userId, subscriptionId);
+            break;
+        default:
+            break;
+    }
+}
