1c813e8266c82687948503b97d0a21ed83027d18
iDarkQ <matikuki3@gmail.com>
2025-11-26 21:19:56 +0000

    refactor(questions-studies.tsx): allow only admins to create and edit studies

--- app/questions/[[...studyId]]/components/questions-studies.tsx
+++ app/questions/[[...studyId]]/components/questions-studies.tsx
@@ -5,36 +5,55 @@ import { QuestionsEditStudy } from "@/app/questions/[[...studyId]]/components/qu
 import clsx from "clsx";
 import { Link } from "@/app/_components/link";
 import { Study } from "@/app/generated/prisma/browser";
+import { Banner } from "@/app/_components/banner";
+import { fetchLoggedUser } from "@/app/_server/fetch-logged-user";
 
 interface Props {
   studies: Study[];
   selectedStudy?: Study;
 }
 
-export const QuestionsStudies = ({ studies, selectedStudy }: Props) => (
-  <div className="flex flex-col gap-2 items-center justify-center">
-    <div className="flex gap-2 items-center justify-center">
-      {studies.map((study) => (
-        <Link key={study.id} link={`/questions/${study.id}`} hideStyles>
-          <Chip
-            clickable
-            variant={selectedStudy?.id === study.id ? "contained" : "outlined"}
-          >
-            <Text
-              as="p"
-              className={clsx(selectedStudy?.id === study.id && "text-white")}
-            >
-              {study.title}
-            </Text>
-          </Chip>
-        </Link>
-      ))}
+export const QuestionsStudies = async ({ studies, selectedStudy }: Props) => {
+  const user = await fetchLoggedUser();
+  return (
+    <div className="flex flex-col gap-2 items-center justify-center w-full">
+      <div className="flex gap-2 items-center justify-center w-full">
+        {studies.length > 0 ? (
+          studies.map((study) => (
+            <Link key={study.id} link={`/questions/${study.id}`} hideStyles>
+              <Chip
+                clickable
+                variant={
+                  selectedStudy?.id === study.id ? "contained" : "outlined"
+                }
+              >
+                <Text
+                  as="p"
+                  className={clsx(
+                    selectedStudy?.id === study.id && "text-white"
+                  )}
+                >
+                  {study.title}
+                </Text>
+              </Chip>
+            </Link>
+          ))
+        ) : (
+          <Banner center className="flex w-full z-1">
+            <Text>Não há estudos neste momento</Text>
+          </Banner>
+        )}
+      </div>
+      {user?.admin && (
+        <div className="flex gap-2 items-center justify-center">
+          <QuestionsCreateStudy />
+          {selectedStudy && (
+            <QuestionsEditStudy
+              study={studies.find((study) => study.id === selectedStudy?.id)!}
+            />
+          )}
+        </div>
+      )}
     </div>
-    <div className="flex gap-2 items-center justify-center">
-      <QuestionsCreateStudy />
-      <QuestionsEditStudy
-        study={studies.find((study) => study.id === selectedStudy?.id)!}
-      />
-    </div>
-  </div>
-);
+  );
+};
