48fccae5ed25d2db23ad7fa11e36bd29b1402507
iDarkQ <matikuki3@gmail.com>
2025-11-26 20:54:42 +0000

    refactor(question-creator-dialog.tsx): utilize DialogPart & Combobox for category choice

--- app/questions/[[...studyId]]/components/dialogs/question-creator-dialog.tsx
+++ app/questions/[[...studyId]]/components/dialogs/question-creator-dialog.tsx
@@ -2,18 +2,24 @@
 
 import { AnswerOption } from "@/app/_components/answer-option";
 import { Button } from "@/app/_components/button";
-import { Dialog } from "@/app/_components/dialog";
+import { ComboboxInput } from "@/app/_components/combobox/combobox-input";
+import { ComboboxOption } from "@/app/_components/combobox/combobox-option";
+import { ComboboxOptions } from "@/app/_components/combobox/combobox-options";
+import { Dialog } from "@/app/_components/dialog/dialog";
+import { DialogPart } from "@/app/_components/dialog/dialog-part";
 import { Divider } from "@/app/_components/divider";
 import { Field } from "@/app/_components/field";
 import { Input } from "@/app/_components/input";
 import { Text } from "@/app/_components/text";
 import { useQuestionCreatorDialog } from "@/app/questions/[[...studyId]]/hooks/use-question-creator-dialog";
 import { QuestionWithOptions } from "@/types/question-with-options";
+import { Combobox, RadioGroup } from "@headlessui/react";
 
 interface Props {
   onClose: () => void;
   question?: QuestionWithOptions;
   selectedStudy: string;
+  categories: string[];
 }
 
 const MAX_ANSWERS = 5;
@@ -22,6 +28,7 @@ export const QuestionCreatorDialog = ({
   onClose,
   question: baseQuestion,
   selectedStudy,
+  categories,
 }: Props) => {
   const { options: baseOptions } = baseQuestion ?? {};
   const {
@@ -51,67 +58,80 @@ export const QuestionCreatorDialog = ({
   return (
     <Dialog
       onClose={onClose}
-      title={isEditMode ? "Edit Question" : "Question Creator"}
+      title={isEditMode ? "Alterar Pergunta" : "Criar Pergunta"}
     >
-      <Field>
-        <Text as="label">Question</Text>
-        <Input
-          type="text"
-          placeholder="Question"
-          className="w-full"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-        />
-      </Field>
-
-      <Field>
-        <Text as="label">Category</Text>
-        <Input
-          type="text"
-          placeholder="Category"
-          className="w-full"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        />
-      </Field>
-
-      <Divider />
-
-      <div className="flex flex-col gap-2">
-        {optionsDto.map((opt, idx) => (
-          <AnswerOption
-            key={opt.id}
-            option={optionLetter(idx)}
-            label={opt.content}
-            selected={selectedAnswerId === opt.id}
-            defaultValue={opt.content}
-            editable
-            onDelete={() => removeAnswer(opt.id)}
-            onClick={() => setCorrectAnswer(opt.id)}
-            onChange={(value) => updateOptionContent(opt.id, value)}
+      <DialogPart>
+        <Field>
+          <Text as="label">Pergunta</Text>
+          <Input
+            type="text"
+            placeholder="Question"
+            className="w-full"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
           />
-        ))}
-      </div>
+        </Field>
 
-      <Button variant="outlined" onClick={addAnswer} disabled={!canAddMore}>
-        <Text>
-          {canAddMore
-            ? "Add Option"
-            : `Max answers reached ${optionsDto.length}/${MAX_ANSWERS}`}
-        </Text>
-      </Button>
+        <Field>
+          <Text as="label">Categoria</Text>
+          <Combobox value={category} onChange={(v) => setCategory(v ?? "")}>
+            <ComboboxInput
+              aria-label="Assignee"
+              displayValue={(c) => c}
+              onChange={(event) => setCategory(event.target.value)}
+            />
+            <ComboboxOptions anchor="bottom">
+              {categories.map((person) => (
+                <ComboboxOption key={person} value={person}>
+                  {person}
+                </ComboboxOption>
+              ))}
+            </ComboboxOptions>
+          </Combobox>
+        </Field>
+      </DialogPart>
 
       <Divider />
 
-      <Button onClick={handleCreateOrSave} disabled={!canCreate}>
-        <Text className="text-white!">{isEditMode ? "Save" : "Create"}</Text>
-      </Button>
+      <DialogPart>
+        <RadioGroup className="flex flex-col gap-2">
+          {optionsDto.map((opt, idx) => (
+            <AnswerOption
+              key={opt.id}
+              option={optionLetter(idx)}
+              label={opt.content}
+              selected={selectedAnswerId === opt.id}
+              defaultValue={opt.content}
+              editable
+              onDelete={() => removeAnswer(opt.id)}
+              onClick={() => setCorrectAnswer(opt.id)}
+              onChange={(value) => updateOptionContent(opt.id, value)}
+            />
+          ))}
+        </RadioGroup>
 
-      {isEditMode && (
-        <Button className="mt-2" onClick={handleDelete} variant="warning">
-          <Text className="text-white!">Delete</Text>
+        <Button variant="outlined" onClick={addAnswer} disabled={!canAddMore}>
+          <Text>
+            {canAddMore
+              ? "Adicionar Opção"
+              : `Respostas máximas alcançadas: ${optionsDto.length}/${MAX_ANSWERS}`}
+          </Text>
         </Button>
-      )}
+      </DialogPart>
+
+      <Divider />
+
+      <DialogPart>
+        <Button onClick={handleCreateOrSave} disabled={!canCreate}>
+          <Text className="text-white!">{isEditMode ? "Guardar" : "Criar"}</Text>
+        </Button>
+
+        {isEditMode && (
+          <Button onClick={handleDelete} variant="warning">
+            <Text className="text-white!">Eliminar</Text>
+          </Button>
+        )}
+      </DialogPart>
     </Dialog>
   );
 };
