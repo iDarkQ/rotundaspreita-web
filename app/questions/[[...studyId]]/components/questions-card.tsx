665d74db93fd036c1e644c06b60081fa856f6615
iDarkQ <matikuki3@gmail.com>
2025-11-26 21:16:19 +0000

    refactor(questions-card.tsx): display actual count of how many times user responded to question

--- app/questions/[[...studyId]]/components/questions-card.tsx
+++ app/questions/[[...studyId]]/components/questions-card.tsx
@@ -3,16 +3,27 @@
 import { Card } from "@/app/_components/card";
 import { Text } from "@/app/_components/text";
 import { useRipple } from "@/app/_hooks/use-ripple";
+import { User } from "@/app/generated/prisma/browser";
 import { QuestionCreatorDialog } from "@/app/questions/[[...studyId]]/components/dialogs/question-creator-dialog";
+import { QuestionViewerDialog } from "@/app/questions/[[...studyId]]/components/dialogs/question-viewer-dialog";
 import { QuestionWithOptions } from "@/types/question-with-options";
 import { RefObject, useState } from "react";
 
 interface Props {
   selectedStudy: string;
   question: QuestionWithOptions;
+  answeredCount?: number;
+  categories: string[];
+  user: User;
 }
 
-export const QuestionsCard = ({ question, selectedStudy }: Props) => {
+export const QuestionsCard = ({
+  question,
+  selectedStudy,
+  categories,
+  user,
+  answeredCount = 0,
+}: Props) => {
   const [isOpen, setIsOpen] = useState(false);
   const { events, cancel, ref } = useRipple();
 
@@ -29,11 +40,15 @@ export const QuestionsCard = ({ question, selectedStudy }: Props) => {
         }}
         className="cursor-pointer relative c-border flex flex-col gap-2 overflow-hidden"
       >
-        <div className="absolute top-0 left-0 w-full h-5 bg-primary flex items-center justify-center">
-          <Text as="p" className="text-white!">
-            Já respondeu 2 vezes
-          </Text>
-        </div>
+        {answeredCount > 0 && (
+          <div className="absolute top-0 left-0 w-full h-5 bg-primary flex items-center justify-center">
+            <Text as="p" className="text-white!">
+              Já respondeu{" "}
+              {`${answeredCount} ${answeredCount > 1 ? "vezes" : "vez"}`}
+            </Text>
+          </div>
+        )}
+
         <div className="flex flex-col p-5">
           <Text as="h2">{question.content}</Text>
         </div>
