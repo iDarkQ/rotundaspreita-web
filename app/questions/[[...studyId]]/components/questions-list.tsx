23f38cc773bc219e5da67f106c99e3ed588273e2
iDarkQ <matikuki3@gmail.com>
2025-11-26 21:18:56 +0000

    refactor(questions-list.tsx): display banner with message saying no questions when no questions get provided

--- app/questions/[[...studyId]]/components/questions-list.tsx
+++ app/questions/[[...studyId]]/components/questions-list.tsx
@@ -1,24 +1,45 @@
 "use client";
 
+import { Banner } from "@/app/_components/banner";
+import { Text } from "@/app/_components/text";
+import { User } from "@/app/generated/prisma/browser";
 import { QuestionsCard } from "@/app/questions/[[...studyId]]/components/questions-card";
 import { useManageFetchedQuestions } from "@/app/questions/[[...studyId]]/providers/manage-fetched-questions";
 
 interface Props {
   selectedStudy: string;
+  responses: { [key: string]: number };
+  categories: string[];
+  user: User;
 }
 
-export const QuestionsList = ({ selectedStudy }: Props) => {
+export const QuestionsList = ({
+  selectedStudy,
+  responses,
+  categories,
+  user,
+}: Props) => {
   const { questions } = useManageFetchedQuestions();
 
   return (
-    <div className="overflow-auto grid grid-cols-2 gap-5 w-full">
-      {questions.map((q, index) => (
-        <QuestionsCard
-          key={q.id ?? index}
-          question={q}
-          selectedStudy={selectedStudy}
-        />
-      ))}
-    </div>
+    <>
+      {questions.length < 1 && (
+        <Banner center className="w-full">
+          <Text>Não há questoes neste momento</Text>
+        </Banner>
+      )}
+      <div className="overflow-auto grid grid-cols-2 gap-2 w-full">
+        {questions.map((q, index) => (
+          <QuestionsCard
+            key={q.id ?? index}
+            question={q}
+            selectedStudy={selectedStudy}
+            answeredCount={responses[q.id]}
+            categories={categories}
+            user={user}
+          />
+        ))}
+      </div>
+    </>
   );
 };
