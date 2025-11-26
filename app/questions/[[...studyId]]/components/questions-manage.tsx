283dd220c7c013b0e02f81f6f2cec6b432a0ecb3
iDarkQ <matikuki3@gmail.com>
2025-11-26 21:19:21 +0000

    refactor(questions-manage.tsx): allow only admins to create questions

--- app/questions/[[...studyId]]/components/questions-manage.tsx
+++ app/questions/[[...studyId]]/components/questions-manage.tsx
@@ -1,21 +1,41 @@
+import { Divider } from "@/app/_components/divider";
+import { fetchLoggedUser } from "@/app/_server/fetch-logged-user";
+import { User } from "@/app/generated/prisma/browser";
 import { QuestionsCreator } from "@/app/questions/[[...studyId]]/components/questions-creator";
 import { QuestionsList } from "@/app/questions/[[...studyId]]/components/questions-list";
 import { ManageFetchedQuestionsProvider } from "@/app/questions/[[...studyId]]/providers/manage-fetched-questions";
-import { findStudyQuestions } from "@/services/question-service";
+import {
+  countAnsweredQuestions,
+  findStudyQuestions,
+} from "@/services/question-service";
+import { fetchAllStudyCategories } from "@/services/study-service";
 
 interface Props {
+  user: User;
   studyId?: string;
 }
 
-export const QuestionsManage = async ({ studyId }: Props) => {
+export const QuestionsManage = async ({ user, studyId }: Props) => {
   if (!studyId) return;
 
   const questions = await findStudyQuestions(studyId);
+  const responses = await countAnsweredQuestions();
+  const categories = await fetchAllStudyCategories(studyId);
 
   return (
     <ManageFetchedQuestionsProvider questions={questions}>
-      <QuestionsCreator selectedStudy={studyId} />
-      <QuestionsList selectedStudy={studyId} />
+      <Divider orientation="horizontal" />
+      <div className="flex flex-col gap-2 w-full">
+        {user.admin && (
+          <QuestionsCreator selectedStudy={studyId} categories={categories} />
+        )}
+        <QuestionsList
+          selectedStudy={studyId}
+          responses={responses}
+          categories={categories}
+          user={user}
+        />
+      </div>
     </ManageFetchedQuestionsProvider>
   );
 };
