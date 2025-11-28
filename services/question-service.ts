75fa3f9003ec729c5c2e7811ac60aa7286599945
iDarkQ <matikuki3@gmail.com>
2025-11-28 21:29:52 +0000

    feat(question-service.ts): create new service functions

--- services/question-service.ts
+++ services/question-service.ts
@@ -1,6 +1,8 @@
-import { OptionLetter } from "@/app/generated/prisma/enums";
+  import { OptionLetter } from "@/app/generated/prisma/enums";
 import { UpdateQuestionDto } from "@/lib/dtos/question/update-question-option.dto";
-import { createQuestionQuery, deleteQuestionQuery, findManyQuestionsQuery, findQuestionQuery, updateQuestionQuery } from "@/lib/queries/question";
+import { countQuestionsQuery, createQuestionQuery, deleteQuestionQuery, findManyQuestionsQuery, findQuestionQuery, updateQuestionQuery } from "@/lib/queries/question";
+import { fetchTestResultsQuery } from "@/lib/queries/test-results";
+import { verifySession, verifyAdminPermissions } from "@/services/user-service";
 
 interface OptionProp {
     letter: OptionLetter;
@@ -9,6 +11,9 @@ interface OptionProp {
 }
 
 export const createQuestion = async (studyId: string, content: string, category: string, options: OptionProp[]) => {
+    const session = await verifyAdminPermissions();
+    if (!session) return;
+
     return createQuestionQuery({
         content, category, study: {
             connect: {
@@ -22,6 +27,9 @@ export const createQuestion = async (studyId: string, content: string, category:
 }
 
 export const updateQuestion = async (id: string, content: string, category: string, options: UpdateQuestionDto[]) => {
+    const session = await verifyAdminPermissions();
+    if (!session) return;
+
     const existingOptions = (await findQuestionQuery({ id }));
 
     if (!existingOptions) return;
@@ -48,9 +56,37 @@ export const updateQuestion = async (id: string, content: string, category: stri
 }
 
 export const deleteQuestion = async (id: string) => {
+    const session = await verifyAdminPermissions();
+    if (!session) return;
+
     return deleteQuestionQuery({ id });
 }
 
 export const findStudyQuestions = async (studyId: string) => {
+    const session = await verifySession();
+    if (!session) return [];
+
     return findManyQuestionsQuery({ studyId });
 }
+
+export const countQuestions = async (studyId?: string, category?: string) => {
+    const session = await verifySession();
+    if (!session) return 0;
+
+    return countQuestionsQuery({ category, studyId });
+}
+
+export const countAnsweredQuestions = async () => {
+    const session = await verifySession();
+    if (!session) return {};
+
+    const results = await fetchTestResultsQuery({ userId: session.id });
+
+    const questionids = results.reduce<{ [key: string]: number }>((prev, curr) => {
+        const id = curr.questionId;
+        prev[id] = (prev[id] ?? 0) + 1;
+        return prev;
+    }, {});
+
+    return questionids;
+};
