db67e6412034bae4766aabaebda436247166fa84
iDarkQ <matikuki3@gmail.com>
2025-11-29 18:01:35 +0000

    feat(subscription.ts): add cancel and uncancel functions

--- services/server/subscription.ts
+++ services/server/subscription.ts
@@ -1,13 +1,32 @@
 "use server";
 
-import { createSubscriptionQuery, deleteSubscriptionQuery, fetchSubscriptionQuery } from "@/lib/queries/subscription";
+import { fetchLoggedUser } from "@/app/_server/fetch-logged-user";
+import { createOrReplaceSubscriptionQuery, fetchSubscriptionQuery } from "@/lib/queries/subscription";
 import { stripe } from "@/lib/stripe";
 
-export const startSubscription = async (userId: string, stripeSubId: string) => createSubscriptionQuery({ user: { connect: { id: userId } }, stripeSubId });
-export const cancelSubscription = async (userId: string) => {
-    const subscription = await deleteSubscriptionQuery({ userId })
+export const startSubscription = async (userId: string, stripeSubId: string) => createOrReplaceSubscriptionQuery({ user: { connect: { id: userId } }, stripeSubId, createdAt: new Date(), expiresAt: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) });
 
-    await stripe.subscriptions.cancel(subscription.stripeSubId);
+export const cancelSubscription = async () => {
+    const user = await fetchLoggedUser();
+    if (!user) return;
+
+    const subscription = await fetchUserSubscription(user.id);
+    if (!subscription) return;
+    stripe.subscriptions.update(subscription.stripeSubId, {
+        cancel_at_period_end: true,
+    });
+}
+
+export const uncancelSubscription = async () => {
+    const user = await fetchLoggedUser();
+    if (!user) return;
+
+    const subscription = await fetchUserSubscription(user.id);
+    if (!subscription) return;
+
+    stripe.subscriptions.update(subscription.stripeSubId, {
+        cancel_at_period_end: false,
+    });
 }
 
 export const fetchUserSubscription = async (userId: string) => fetchSubscriptionQuery({ userId });
