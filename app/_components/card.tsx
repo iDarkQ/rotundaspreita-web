b11adab2f86ea5316703cf6221cac55cb9bdb407
iDarkQ <matikuki3@gmail.com>
2025-11-30 18:06:45 +0000

    refactor(card.tsx): add variants

--- app/_components/card.tsx
+++ app/_components/card.tsx
@@ -4,15 +4,18 @@ import { forwardRef, HTMLAttributes, ReactNode } from "react";
 interface Props extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
   className?: string;
+  variant?: "primary" | "secondary";
 }
 
 export const Card = forwardRef<HTMLDivElement, Props>(
-  ({ children, className, ...rest }, ref) => {
+  ({ children, className, variant = "primary", ...rest }, ref) => {
     return (
       <div
         ref={ref}
         className={clsx(
-          "relative bg-white shadow-lg flex flex-col c-border p-5",
+          "relative flex flex-col p-5",
+          variant === "primary" && "c-border shadow-lg bg-white",
+          variant === "secondary" && "rounded-sm bg-card-secondary",
           className
         )}
         {...rest}
