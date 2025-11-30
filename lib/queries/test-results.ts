f54df66456b831a64ae70ae50966eb511d17a7ac
iDarkQ <matikuki3@gmail.com>
2025-11-30 18:12:06 +0000

    feat(test-results.ts): add updateManyTestResultsQuery

--- lib/queries/test-results.ts
+++ lib/queries/test-results.ts
@@ -6,3 +6,5 @@ import prisma from "@/lib/prisma";
 export const createTestResultQuery = async (data: Prisma.TestResultCreateInput) => prisma.testResult.create({ data });
 
 export const fetchTestResultsQuery = async (where: Prisma.TestResultWhereInput) => prisma.testResult.findMany({ where });
+
+export const updateManyTestsResultsQuery = async (where: Prisma.TestResultWhereInput, data: Prisma.TestResultUpdateManyMutationInput) => prisma.testResult.updateMany({ where, data });
