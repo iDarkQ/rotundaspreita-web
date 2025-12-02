4a49c2eb3c3764b2e7d5435417ad1714d900ba77
iDarkQ <matikuki3@gmail.com>
2025-12-02 00:33:30 +0000

    refactor: change from prisma postgres to global postgres

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@/app/generated/prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
-}
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-const prisma = globalForPrisma.prisma || new PrismaClient({ accelerateUrl: process.env.DATABASE_URL ?? "" }).$extends(withAccelerate())
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter }).$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
