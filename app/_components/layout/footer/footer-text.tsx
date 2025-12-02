b59ea27403b6feb72729f284f4c900d77ae73ab4
iDarkQ <matikuki3@gmail.com>
2025-12-02 00:17:33 +0000

    refactor(footer-text.tsx): translate footer

--- app/_components/layout/footer/footer-text.tsx
+++ app/_components/layout/footer/footer-text.tsx
@@ -2,6 +2,6 @@ import { Text } from "@/app/_components/text";
 
 export const FooterText = () => (
   <Text as="p" className="text-center text-2xl font-bold text-white">
-    Copyright © ROTUNDÁSPREITA | All rights reserved
+    Copyright © ROTUNDÁSPREITA | Todos os direitos reservados
   </Text>
 );
