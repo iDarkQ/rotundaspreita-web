b0bcd26dc08498cbe94c866e98e3775f5c4d647d
iDarkQ <matikuki3@gmail.com>
2025-12-03 20:51:53 +0000

    refactor(faq): simplify & improve SRP

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,6 +1,6 @@
 import { Section } from "@/app/_components/section";
-import { FAQQuestions } from "@/app/faq/faq-questions";
-import { FAQTitle } from "@/app/faq/faq-title";
+import { FAQQuestions } from "@/app/faq/components/faq-questions";
+import { FAQTitle } from "@/app/faq/components/faq-title";
 
 export default async function FAQ() {
   return (
