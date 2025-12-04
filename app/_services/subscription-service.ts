ca9cdde7f7480b6a64a6243bf50c1c08b7c4e609
iDarkQ <matikuki3@gmail.com>
2025-12-04 15:45:31 +0000

    fix: update subscriptions status when user changes it

--- app/_services/subscription-service.ts
+++ app/_services/subscription-service.ts
@@ -3,6 +3,7 @@
 import { serverFetchUserSubscription } from "@/app/_services/server/subscription-service";
 import { stripe } from "@/app/_lib/stripe";
 import { verifySession } from "@/app/_services/user-service";
+import { updateSubscriptionQuery } from "@/app/_lib/queries/subscription";
 
 export const fetchLoggedUserSubscription = async () => {
   const session = await verifySession();
@@ -14,7 +15,9 @@ export const cancelSubscription = async () => {
   const session = await verifySession();
 
   const subscription = await serverFetchUserSubscription(session.id);
-  if (!subscription) return;
+  if (!subscription || !subscription.stripeSubId) return;
+
+  await updateSubscriptionQuery({ id: subscription.id }, { cancelled: true })
   stripe.subscriptions.update(subscription.stripeSubId, {
     cancel_at_period_end: true,
   });
@@ -24,7 +27,8 @@ export const uncancelSubscription = async () => {
   const session = await verifySession();
 
   const subscription = await serverFetchUserSubscription(session.id);
-  if (!subscription) return;
+  if (!subscription || !subscription.stripeSubId) return;
+  await updateSubscriptionQuery({ id: subscription.id }, { cancelled: false })
 
   stripe.subscriptions.update(subscription.stripeSubId, {
     cancel_at_period_end: false,
