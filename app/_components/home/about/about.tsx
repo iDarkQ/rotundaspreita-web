f9cf6affacc04a3feb1813027486a77ff56f6d73
iDarkQ <matikuki3@gmail.com>
2025-12-07 11:29:09 +0000

    refactor(about.tsx): add missing about heading

--- app/_components/home/about/about.tsx
+++ app/_components/home/about/about.tsx
@@ -2,11 +2,13 @@ import { AboutBlobs } from "@/app/_components/home/about/about-blobs";
 import { AboutFeatureAnalytics } from "@/app/_components/home/about/about-features/about-feature-analytics";
 import { AboutFeatureManyQuestions } from "@/app/_components/home/about/about-features/about-feature-many-questions";
 import { AboutFeatureTestMenu } from "@/app/_components/home/about/about-features/about-feature-test-menu";
+import { AboutHeading } from "@/app/_components/home/about/about-heading";
 import { Section } from "@/app/_components/section";
 
 export const HomeAbout = () => (
   <Section>
     <AboutBlobs />
+    <AboutHeading />
     <div className="z-1 flex w-full flex-col gap-5">
       <div className="flex w-full flex-col gap-20">
         <AboutFeatureTestMenu />
