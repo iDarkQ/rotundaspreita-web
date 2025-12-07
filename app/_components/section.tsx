4db76a932a3f3d49062c5810099e36c6d36b9733
iDarkQ <matikuki3@gmail.com>
2025-12-07 11:30:21 +0000

    refactor(section): move section width into layers components

--- app/_components/section.tsx
+++ app/_components/section.tsx
@@ -10,8 +10,7 @@ export const Section = ({ children, className }: SectionProps) => (
   <section className="bg-background flex w-full flex-1 justify-center">
     <div
       className={clsx(
-        "relative flex flex-col items-center justify-start gap-5",
-        "mx-auto w-[90%]! max-w-(--breakpoint-2xl) max-md:w-[95%]!",
+        "section-width relative flex flex-col items-center justify-start gap-5",
         className && className,
       )}
     >
