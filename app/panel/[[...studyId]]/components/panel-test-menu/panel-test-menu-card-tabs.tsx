b74bf5e60d6714c5805897eccb98bcaaeb306217
iDarkQ <matikuki3@gmail.com>
2025-12-08 20:29:31 +0000

    refactor(panel-test-menu-card-tabs.tsx): add "Errados" tab & remove comments

--- app/panel/[[...studyId]]/components/panel-test-menu/panel-test-menu-card-tabs.tsx
+++ app/panel/[[...studyId]]/components/panel-test-menu/panel-test-menu-card-tabs.tsx
@@ -16,43 +16,12 @@ export const PanelTestMenuCardTabs = () => {
 
   return (
     <TabGroup onChange={handleOnChange}>
-      <TabList className="w-full">
-        <Tab label="Todos" />
-        <Tab label="Novos" />
-        <Tab label="Difíceis" />
+      <TabList className="flex w-full flex-wrap gap-2">
+        <Tab label="Todos" className="min-w-[100px]" />
+        <Tab label="Novos" className="min-w-[100px]" />
+        <Tab label="Difíceis" className="min-w-[100px]" />
+        <Tab label="Errados" className="min-w-[100px]" />
       </TabList>
     </TabGroup>
   );
 };
-
-/*
-
-"use client";
-
-import { Tab } from "@/app/_components/tab";
-import { TabList } from "@/app/_components/tab-list";
-import { useTestMenu } from "@/app/panel/[[...studyId]]/providers/test-menu";
-import { Difficulty } from "@/app/_types/difficulty";
-import { TabGroup } from "@headlessui/react";
-
-export const PanelTestMenuCardTabs = () => {
-  const { setDifficulty } = useTestMenu();
-
-  const handleOnChange = (index: number) => {
-    const difficulties = Object.values(Difficulty);
-    setDifficulty(difficulties[index]);
-  };
-
-  return (
-    <TabGroup onChange={handleOnChange}>
-      <TabList className="w-full">
-        <Tab label="Todos" />
-        <Tab label="Novos" />
-        <Tab label="Difíceis" />
-      </TabList>
-    </TabGroup>
-  );
-};
-
-
- */
