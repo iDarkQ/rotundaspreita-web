35e503d0800d1a98e8f2e11e8d28a13c9766b783
iDarkQ <matikuki3@gmail.com>
2025-12-11 13:20:27 +0000

    refactor(page.tsx): change title and description of privacy policy

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -2,6 +2,13 @@ import { Markdown } from "@/app/_components/markdown";
 import { Section } from "@/app/_components/section";
 import { PrivacyPolicyTitle } from "@/app/privacy-policy/components/privacy-policy-title";
 import { privacyPolicyMarkdown } from "@/app/privacy-policy/data/privacy-policy";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Política de Privacidade",
+  description:
+    "Leia nossa Política de Privacidade para entender como coletamos, usamos e protegemos suas informações.",
+};
 
 export default async function PrivacyPolicy() {
   return (
