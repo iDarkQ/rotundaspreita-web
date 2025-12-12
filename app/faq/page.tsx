6e2608f765289761c174c7fac484d019df86644b
iDarkQ <matikuki3@gmail.com>
2025-12-12 08:04:10 +0000

    feat(page.tsx): add title to FAQ page

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -4,6 +4,7 @@ import { FAQTitle } from "@/app/faq/components/faq-title";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
+  title: "Perguntas Frequentes",
   description:
     "Encontre respostas para dúvidas comuns sobre os exames, funcionalidades da plataforma e como se tornar instrutor ou diretor de escola de condução.",
 };
