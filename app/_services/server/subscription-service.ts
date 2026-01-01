7de536099be23b8a03169072cb803eaaefc87927
iDarkQ <matikuki3@gmail.com>
2026-01-01 11:25:56 +0000

    refactor(subscription-service.ts): allow extendTo to be passed in startSubscription action

--- app/_services/server/subscription-service.ts
+++ app/_services/server/subscription-service.ts
@@ -2,16 +2,22 @@
 
 import {
   createOrReplaceSubscriptionQuery,
+  deleteManySubscriptionQuery,
   fetchSubscriptionQuery,
+  updateManySubscriptionQuery,
 } from "@/app/_lib/queries/subscription";
 
 export const serverStartSubscription = async (
   userId: string,
   stripeSubId: string,
+  extendTo?: number,
 ) =>
   createOrReplaceSubscriptionQuery({
     user: { connect: { id: userId } },
     stripeSubId,
+    expiresAt: extendTo ? new Date(extendTo * 1000) : undefined,
+  });
+
 export const serverRenewSubscription = async (subId: string, extendTo: number) =>
   updateManySubscriptionQuery(
     { stripeSubId: subId },
